6f5e8c9bfa7561d62a16e9e6f4133eb249b1c92e
yorin <werbyvillarino@gmail.com>
2015-05-16 08:56:09 +0800

    05/16/2015

--- run.py
+++
@@ -1,5 +0,0 @@
-"""
-http://stackoverflow.com/questions/13437402/how-to-run-scrapy-from-within-a-python-script
-"""
-
-os.system("scrapy crawl yourspider")
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/werby/videofarm/videofarm/
-PATH=$PATH:/usr/local/bin
-export PATH
-scrapy crawl youtube -a query=6KZ1kvIUFrU
-cd -
-exit 0

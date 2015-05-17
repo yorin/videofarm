17bb9da22b8af973f8dc744279cb1442ea817891
yorin <werbyvillarino@gmail.com>
2015-05-17 19:14:24 +0800

    05/17/2015 fixed issues running the script on full directory for cron

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
-export QUERY="$1"
-cd ../videofarm/videofarm/spiders/
+export QUERY="$2"
+export SPIDER="$1"
+DIRPAT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+#echo $DIRPAT
+cd $DIRPAT/videofarm/spiders/
 PATH=$PATH:/usr/local/bin
 export PATH
 function scraperon {
 #scrapy crawl youtube -a query=6KZ1kvIUFrU
-scrapy crawl youtube -a query=$QUERY -o ../spiders/cache/$QUERY.json
+scrapy crawl "$SPIDER" -a query="$QUERY" -o ../spiders/cache/"$QUERY".json
 }
 function curly {
 LINKER=$(cat ../spiders/cache/$QUERY.json |grep -Po '"urlstream": "(.*?)"'|sed -e "s/\"//g"|sed -e "s/urlstream\: //g")
@@ -16,6 +18,8 @@ LINKTITLE=$(cat ../spiders/cache/$QUERY.json |grep -Po '"youtubetitle": "(.*?)"'
  if [ ! -f "../downloads/$LINKTITLE.mp4" ] && [ ! -s "../downloads/$LINKTITLE.mp4" ] && [ ! -e "../downloads/$LINKTITLE.mp4" ]; then
    curl "$LINKER" -a -k -o "../downloads/$LINKTITLE.mp4" --limit-rate 56k
    #curl http://r2---sn-2aqu-hoad.googlevideo.com/videoplayback?ratebypass=yes&mime=video%2Fmp4&expire=1431708337&initcwndbps=1101250&sver=3&dur=744.663&ipbits=0&pl=20&itag=18&mm=31&ip=122.52.86.12&key=yt5&ms=au&source=youtube&upn=mkTwVJ0pWNg&mv=m&mt=1431686625&id=o-AGQqw48F-lzy8VsUTG3QmaWbhiPkpXHtupvOgjMLjV1f&fexp=9407478%2C9408142%2C9408710%2C945137%2C948124%2C952612%2C952637%2C952640%2C952642&signature=4BC91A710F1E0364F867AE3B12A7569D10EAD26F.B610544A016E56E4DA2BD47D1B466F875EB90F68&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Cmime%2Cmm%2Cms%2Cmv%2Cpl%2Cratebypass%2Csource%2Cupn%2Cexpire -a -k -o test.mp4
+ else
+   echo "$LINKTITLE.mp4 has already been downloaded!"
  fi
 }
  CDATEEPOCH=$(date +%s) #UTC datetime
@@ -26,12 +30,12 @@ LINKTITLE=$(cat ../spiders/cache/$QUERY.json |grep -Po '"youtubetitle": "(.*?)"'
  fi
    DIFF=$(($CACHEFDATE - $CDATEEPOCH))
    if [ "$DIFF" -le "0" ] || [ "$CACHEFDATE" -eq "0" ]; then
-     echo "still a day old $DIFF $CACHEFDATE $CDATEEPOCH"
+     #echo $DIFF $CACHEFDATE $CDATEEPOCH
      curly
    else
      rm -rf ../spiders/cache/$QUERY.json
      scraperon
      curly
    fi
-cd -
+cd - > /dev/null 2>&1
 exit 0
